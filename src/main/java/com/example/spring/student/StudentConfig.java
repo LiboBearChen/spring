47441141d41aee0acca3af11f31bbe12a8b4dd96
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-05-20 20:22:15 -0400

    update

--- src/main/java/com/example/spring/student/StudentConfig.java
+++ src/main/java/com/example/spring/student/StudentConfig.java
@@ -1,15 +1,22 @@
 package com.example.spring.student;
 
+import com.sun.tools.javac.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+
 
 @Configuration
 public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository){
       return args->{
-
+            Student s1= new Student("ewfw","eewfewf", LocalDate.of(2000, Month.JANUARY,2),22);
+          Student s2= new Student("ewfw222","eewfewf", LocalDate.of(2000, Month.JANUARY,2),22);
+          studentRepository.saveAll(List.of(s1,s2));
       };
     }
 }
